5ddb335cc3efff22c303b2b8abb4f29cf9d634e6
pywind <piwndy21@gmail.com>
2023-06-27 06:45:23 +0000

    add dev container

--- examples/python/wordcount.py
+++ examples/python/wordcount.py
@@ -5,21 +5,14 @@ from pyspark.sql import SparkSession
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: wordcount <file>", file=sys.stderr)
-        sys.exit(-1)
 
     spark = SparkSession\
         .builder\
         .appName("PythonWordCount")\
         .getOrCreate()
 
-    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
-    counts = lines.flatMap(lambda x: x.split(' ')) \
-                  .map(lambda x: (x, 1)) \
-                  .reduceByKey(add)
-    output = counts.collect()
-    for (word, count) in output:
-        print("%s: %i" % (word, count))
+    df = spark.read.option("delimiter","\t") \
+        .option("header","false") \
+        .csv("/workspaces/docker-spark/examples/u.data").show()
 
     spark.stop()
